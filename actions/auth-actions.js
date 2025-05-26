1ede3cf178205a1654e9e93b0f9ccb76d9e65730
VitaliPri <vitaliy.prymak@cix.csi.cuny.edu>
2025-05-26 15:32:05 -0400

    feat: validating user

---
+++ actions/auth-actions.js
@@ -0,0 +1,25 @@
+"use server";
+
+// server actions are async functions
+
+export async function signup(prevState, formData) {
+  const email = formData.get("email");
+  const password = formData.get("password");
+
+  let errors = {};
+
+  if (!email.includes("@")) {
+    errors.email = "Please enter a valid email address!";
+  }
+
+  if (password.length < 8) {
+    errors.password = "Password must be at least 8 charchters";
+  }
+
+  if (Object.keys(errors).length > 0) {
+    return {
+      errors,
+    };
+  }
+  //   store in db (create a new user)
+}
