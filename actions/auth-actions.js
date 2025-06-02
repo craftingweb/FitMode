cfdfe3d2980f0555de368b9a913585da9d71431b
VitaliPri <vitaliy.prymak@cix.csi.cuny.edu>
2025-06-01 20:00:56 -0400

    feat: server actions

--- actions/auth-actions.js
+++ actions/auth-actions.js
@@ -71,3 +71,8 @@ export async function login(prevState, formData) {
   await createAuthSession(existingUser.id);
   redirect("/training");
 }
+
+export async function auth(mode, prevState, formData) {
+  if (mode === "login") return login(prevState, formData);
+  return signup(prevState, formData);
+}
